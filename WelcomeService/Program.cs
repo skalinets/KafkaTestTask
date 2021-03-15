050f1d31ca73f340908958dfd4f8498b45a40036
Serhiy Kalinets <kalinets@gmail.com>
2021-03-15 14:19:28 +0200

    ready for action

---
+++ WelcomeService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WelcomeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
